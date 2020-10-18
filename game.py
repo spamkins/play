f8763656da96f66abd25fc461c46ba5c4cb36853
Rikki Hodder <r.hodder@novafutur.com>
2020-10-18 13:30:21 +0100

    added first game loop

--- game.py
+++ game.py
@@ -16,3 +16,14 @@ SCREEN_HEIGHT: int = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+        elif event.type == QUIT:
+            running = False
+
