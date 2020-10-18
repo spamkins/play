7670e6e7dcf56acbca7d29679400257bc5d7d1ce
Rikki Hodder <r.hodder@novafutur.com>
2020-10-18 16:21:23 +0100

    added enemy class

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
-import pygame
+import random
+
+import pygame as pg
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -9,17 +11,69 @@ from pygame.locals import (
     QUIT,
 )
 
-pygame.init()
-
 SCREEN_WIDTH: int = 800
 SCREEN_HEIGHT: int = 600
+BG_COLOUR = pg.Color(80, 60, 70)
+PLAYER_COLOUR = pg.Color(90, 140, 190)
+ENEMY_COLOUR = pg.Color(190, 90, 210)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pg.Surface((75, 25))
+        self.surf.fill(PLAYER_COLOUR)
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+
+class Enemy(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pg.Surface((20, 10))
+        self.surf.fill(ENEMY_COLOUR)
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+
+pg.init()
+
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pg.time.Clock()
+
+player = Player()
 
 running = True
 
 while running:
-    for event in pygame.event.get():
+    for event in pg.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
@@ -27,18 +81,14 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    pressed_keys = pg.key.get_pressed()
 
-    surf = pygame.Surface((50, 50))
+    player.update(pressed_keys)
 
-    surf.fill((255, 0, 0))
-    rect = surf.get_rect()
+    screen.fill(BG_COLOUR)
+    screen.blit(player.surf, player.rect)
 
-    surf_centre = (
-        (SCREEN_WIDTH - surf.get_width()) / 2,
-        (SCREEN_HEIGHT - surf.get_height()) / 2
-    )
-
-    screen.blit(surf, surf_centre)
-    pygame.display.flip()
+    # Update the display
+    pg.display.flip()
+    clock.tick(60)
 
